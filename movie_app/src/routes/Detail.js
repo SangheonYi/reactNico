60f12efa03d541229273e33767c1294ef6250b6b
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2021-04-04 23:17:07 +0900

    refactor: Modify redirect url

--- movie_app/src/routes/Detail.js
+++ movie_app/src/routes/Detail.js
@@ -4,13 +4,18 @@ import React from "react";
 class Detail extends React.Component {
 	componentDidMount() {
 		const { location, history } = this.props;
+
 		console.log(location.state);
 		if (location.state == undefined) {
 			history.push("/");
 		}
 	}
 	render() {
-		return <span>Detail</span>;
+		const { location } = this.props;
+
+		if (location.state) {
+			return <span>{location.state.title}</span>;
+		} else return null;
 	}
 }
 export default Detail;
