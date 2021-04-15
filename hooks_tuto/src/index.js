4952143856cbb6d04aacaf21a4b049fdcef4f8dc
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2021-04-15 22:27:31 +0900

    feat: Replace useInput part 1

--- hooks_tuto/src/index.js
+++ hooks_tuto/src/index.js
@@ -1,39 +1,23 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+const useInput = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = event => {
+    console.log(event.target);
+  }
+  return { value, onChange };
+};
+
 const App = () => {
-  const [item, setItem] = useState(1);
-  const incrementItem = () => setItem(item + 1);
-  const decrementItem = () => setItem(item - 1);
+  const name = useInput("Mr.");
   return (
     <div className="App">
-      <h1>Hello {item}</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <button onClick={incrementItem}>"Increment"</button>
-      <button onClick={decrementItem}>"Decrement"</button>
+      <h1>Hello </h1>
+      <input placeholder="Name" {...name}/>
     </div>
   );
 };
-
-class AppUgly extends React.Component {
-  state = {
-    item: 1
-  };
-  incrementItem = () => this.setState({ item: this.state.item + 1 });
-  decrementItem = () => this.setState({ item: this.state.item - 1 });
-
-  render() {
-    const { item } = this.state;
-    return (
-      <div className="App">
-        <h1>Hello {item}</h1>
-        <h2>Start editing to see some magic happen!</h2>
-        <button onClick={this.incrementItem}>"Increment"</button>
-        <button onClick={this.decrementItem}>"Decrement"</button>
-      </div>
-    );
-  }
-}
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
