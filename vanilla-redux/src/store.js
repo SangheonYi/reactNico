1e32700409e633fb9e0acb7a2f166084c21213e3
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2021-05-02 23:06:17 +0900

    refactor: Replace reducer with createReducer

--- vanilla-redux/src/store.js
+++ vanilla-redux/src/store.js
@@ -1,9 +1,9 @@
 import { createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
-
+/*
 const reducer = (state = [], action) => {
   switch (action.type) {
     case addToDo.type:
@@ -13,7 +13,19 @@ const reducer = (state = [], action) => {
     default:
       return state;
   }
-};
+}; */
+
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    // Since immer & toolkit returned new mutated state to redux
+    // I don't need to return new state myself
+    // If return mutated state, Error
+    state.push({ text: action.payload, id: Date.now() });
+  },
+  [deleteToDo]: (state, action) =>
+    // I return a new state, It's OK
+    state.filter((toDo) => toDo.id !== action.payload),
+});
 
 const store = createStore(reducer);
 
